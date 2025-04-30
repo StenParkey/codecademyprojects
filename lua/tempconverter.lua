c80070849a996c72cebec25383957f45742b2111
dogsalt_ <jpstennes@gmail.com>
2025-04-29 18:50:10 -0600

    added temperature converter codecademy project

---
+++ lua/tempconverter.lua
@@ -0,0 +1,39 @@
+function fahrenheitToCelsius(degrees)
+    offsetDegrees = degrees - 32
+    conversionFactor = 5 / 9
+    return offsetDegrees * conversionFactor
+  end
+  
+  function fahrenheitToKelvin(degrees)
+    degreesCelsius = fahrenheitToCelsius(degrees)
+    return degreesCelsius + 273.15
+  end
+  
+  function fahrenheitToCelsius(degrees)
+    offsetDegrees = degrees - 32
+    conversionFactor = 5 / 9
+    return offsetDegrees * conversionFactor
+  end
+  
+  function fahrenheitToKelvin(degrees)
+    degreesCelsius = fahrenheitToCelsius(degrees)
+    return degreesCelsius + 273.15
+  end
+  
+  function convertTemperature(targetUnit, degrees)
+    correctedString = string.lower(targetUnit)
+    
+    if correctedString == 'kelvin' then
+      return fahrenheitToKelvin(degrees)
+    elseif correctedString == 'celsius' then
+      return fahrenheitToCelsius(degrees)
+    else
+      print('Invalid Input')
+      return nil
+    end
+  end
+  
+  inputDegrees = 23
+  convertedDegrees = convertTemperature('Celsius', inputDegrees)
+  print(convertedDegrees)
+  
